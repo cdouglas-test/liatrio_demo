fdbeaea9139ae98a5ef8671b233770a0e4d04611
Charles Douglas <cdouglas.threave@gmail.com>
2025-11-08 18:38:08 -0600

    UPDATES

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -4,7 +4,7 @@
 param(
     [string]$AwsRegion = "us-east-1",
     [string]$EcrRepository = "liatrio-demo-dev-api",
-    [string]$EksClusterName = "liatrio-demo-dev-eks",
+    [string]$EksClusterName = "liatrio-demo-dev-eks-u58x74nr",
     [string]$ImageTag = "manual-$(Get-Date -Format 'yyyyMMddHHmmss')"
 )
 
