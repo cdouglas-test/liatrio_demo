7151f346f21557606a0750f16e1fda180973d8ed
Charles Douglas <cdouglas.threave@gmail.com>
2025-11-10 20:04:29 -0600

    Feature/container versioning (#28)
    * Consolidate README
    
    * Adding Semantic Release

---
+++ .releaserc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  branches: [
+    'main',
+    {
+      name: 'develop',
+      prerelease: 'beta'
+    }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: []
+      }
+    ]
+  ]
+};
